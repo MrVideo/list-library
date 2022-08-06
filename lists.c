f6ff3fa5e09eed58ffbfaa51412b9f60c507d8d9
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2022-08-06 13:57:45 +0200

    Add sorting algorithm

--- lists.c
+++ lists.c
@@ -49,3 +49,54 @@ void print(list l) {
         print(l -> next);
     }
 }
+
+list add_sort(list l, char *new_data) {
+    list prev = NULL, curr = l;
+
+    //If the list is empty, add the element on top.
+    if(l == NULL) {
+        list tmp = malloc(sizeof(struct node));
+        tmp -> data = new_data;
+        tmp -> next = l;
+        return tmp;
+    } else {
+        //If the list is not empty
+        while(curr != NULL) {
+            //If new_data comes before the current node's data then add it before curr
+            if(strcmp(new_data, curr -> data) < 0) {
+                list tmp = malloc(sizeof(struct node));
+                tmp -> data = new_data;
+                tmp -> next = curr;
+                //If the item goes in the first position, do not try to connect the previous node to it
+                //Otherwise, do it
+                if(prev != NULL) {
+                    prev -> next = tmp;
+                    return l;
+                } else return tmp;
+                //If the strings are the same, add it in the next node (it keeps the algorithm stable)
+            } else if(strcmp(new_data, curr -> data) == 0) {
+                prev = curr;
+                curr = curr -> next;
+                list tmp = malloc(sizeof(struct node));
+                tmp -> data = new_data;
+                tmp -> next = curr;
+                prev -> next = tmp;
+                return l;
+                //Otherwise, just keep searching the list
+            } else {
+                prev = curr;
+                curr = curr -> next;
+            }
+        }
+        //If the list is over and nothing was added to it, the item goes in last
+        if(curr == NULL && prev != NULL) {
+            list tmp = malloc(sizeof(struct node));
+            tmp -> data = new_data;
+            tmp -> next = curr;
+            prev -> next = tmp;
+            return l;
+        }
+    }
+    //This gets executed in case of an error
+    return NULL;
+}
--- lists.h
+++ lists.h
@@ -16,5 +16,6 @@ list add(list l, char *new_data);
 list delete(list l, char *to_delete);
 list search(list l, char *query);
 void print(list l);
+list add_sort(list l, char *new_data);
 
 #endif //LISTS_H
